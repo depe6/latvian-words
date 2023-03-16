6e978b234307a69340ef19c1b4cd7bbf93c38bd7
depe6 <depe66@gmail.com>
2023-03-16 22:20:42 +0200

    Added verbs filtering

--- src/app/components/verbs/verbs.component.ts
+++ src/app/components/verbs/verbs.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
 import { from, mergeMap, of } from 'rxjs';
 import { VerbService } from 'src/app/services';
 import { Verb, VerbDialogData } from '../../models';
@@ -22,12 +23,16 @@ import { VerbDialogComponent } from './verb-dialog.component';
                     Add New
                 </button>
             </div>
+            <mat-form-field class="filter">
+                <mat-label>Filter</mat-label>
+                <input matInput (keyup)="applyFilter($event)" #input />
+            </mat-form-field>
             <div class="table-container mat-elevation-z8">
                 <table mat-table [dataSource]="dataSource">
                     <ng-container matColumnDef="position">
                         <th mat-header-cell *matHeaderCellDef>No.</th>
                         <td mat-cell *matCellDef="let verb">
-                            {{ dataSource.indexOf(verb) + 1 }}
+                            {{ dataSource.data.indexOf(verb) + 1 }}
                         </td>
                     </ng-container>
 
@@ -94,6 +99,9 @@ import { VerbDialogComponent } from './verb-dialog.component';
                 max-height: 700px;
                 overflow: auto;
             }
+            .filter {
+                width: 100%;
+            }
         `,
     ],
 })
@@ -105,10 +113,42 @@ export class VerbsComponent implements OnInit {
         'tags',
         'actions',
     ];
-    dataSource: Verb[] = [];
+    dataSource = new MatTableDataSource([] as Verb[]);
     loading = false;
 
-    constructor(private dialog: MatDialog, private verbService: VerbService) {}
+    constructor(private dialog: MatDialog, private verbService: VerbService) {
+        this.dataSource.filterPredicate = (verb: Verb, filter: string) => {
+            if ((filter || '').length === 0) {
+                return true;
+            }
+
+            const verbString = this.toPropString(verb);
+            const verbNormalized = verbString
+                .normalize('NFD')
+                .replace(/\p{Diacritic}/gu, '');
+
+            return verbNormalized.toLowerCase().includes(filter.toLowerCase());
+        };
+    }
+
+    private toPropString(obj: any): string {
+        if (obj === null || obj === undefined) {
+            return '';
+        }
+
+        if (typeof obj === 'object') {
+            const propValues: string[] = [];
+            for (const prop in obj) {
+                if (prop?.toLowerCase() === 'id') {
+                    continue;
+                }
+                propValues.push(this.toPropString(obj[prop]));
+            }
+            return propValues.join('');
+        }
+
+        return obj.toString();
+    }
 
     ngOnInit(): void {
         this.reload();
@@ -119,6 +159,11 @@ export class VerbsComponent implements OnInit {
             await this.verbService.updateVerb(updatedVerb);
             this.reload();
         });
+    }
+
+    applyFilter(event: Event) {
+        const filterValue = (event.target as HTMLInputElement).value;
+        this.dataSource.filter = filterValue.trim().toLowerCase();
     }
 
     async onDeleteVerbClick(verb: Verb) {
@@ -142,7 +187,7 @@ export class VerbsComponent implements OnInit {
             maxWidth: '600px',
             data: {
                 verb,
-                allVerbs: this.dataSource,
+                allVerbs: this.dataSource.data,
                 addingNewVerb,
             } as VerbDialogData,
         });
@@ -158,7 +203,8 @@ export class VerbsComponent implements OnInit {
         this.loading = true;
 
         try {
-            this.dataSource = await this.verbService.loadAllVerbs();
+            const verbs = await this.verbService.loadAllVerbs();
+            this.dataSource.data = verbs;
         } finally {
             this.loading = false;
         }
